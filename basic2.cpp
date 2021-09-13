8956cb2d4e17723c622d3c7dc87bbc44c2d20438
unknown <nibbisaid@gmail.com>
2021-09-14 00:25:46 +0530

    first linked list commit

---
+++ basic2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node(int data){
+        this->data=data;
+        next=NULL;
+    }
+};
+node* takeInput(){
+    
+}
+void print(node* head){
+    while(head!=NULL){
+        cout<<head->data<<"->";
+        head=head->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    // node n1(10);
+    // node* head=&n1;
+    // node n2(20);
+    // node n3(30);
+    // node n4(40);
+    // node n5(50);
+
+    // n1.next=&n2;
+    // n2.next=&n3;
+    // n3.next=&n4;
+    // n4.next=&n5;
+
+    // cout<<head<<endl;
+
+    // print(head);
+
+    // cout<<head<<endl;
+
+    node* n1=new node(10);
+    node* head=n1;
+
+    node* n2=new node(20);
+    node* n3=new node(30);
+    node*n4=new node(40);
+
+    n1->next=n2;
+    n2->next=n3;
+    n3->next=n4;
+
+    print(head);
+
+    return 0;
+}
