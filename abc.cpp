8bacb5e18c49bdf33684d956af4bb145ab796a84
unknown <nibbisaid@gmail.com>
2021-09-14 10:49:11 +0530

    hurried commit

--- abc.cpp
+++ abc.cpp
@@ -2,45 +2,39 @@
 using namespace std;
 
 class node{
-    public:
-    int data;
-    node* next;
-    node(int data){
-        this->data=data;
-    }
+	public:
+	int data;
+	node* next;
+	node(int data){
+		this->data=data;
+
+	}
 };
-
 node* takeInput(){
-int data;
-cin>>data;
-node* head=NULL;
-while(data!=-1){
-    node* newnode=new node(data);
-    if(head==NULL){
-        head=newnode;
-    }
-    else{
-        node* temp=head;
-        while(temp->next!=NULL){
-            temp=temp->next;
-        }
-        temp->next=newnode;
-    }
-    cin>>data;
-}
-return head;
+	int data;
+	cin>>data;
+	node* head=NULL;
+	node* tail=NULL;
+	while(data!=-1){
+		node* newnode=new node(data);
+		if(head==NULL){
+			head=newnode;
+			tail=newnode;
+		}
+		else
+	{
+		tail->next=newnode;
+		tail=tail->next;
+	}
+	cin>>data;
+	}
+	return head;
 }
 
-void print(node* head){
-    while(head!=NULL){
-        cout<<head->data<<" ";
-        head=head->next;
-    }
-}
+
 int main(){
 
-    node* head=takeInput();
-    print(head);
+node* head=takeInput();
 
-    return 0;
+	return 0;
 }
--- basic3.cpp
+++ basic3.cpp
@@ -7,6 +7,7 @@ class node{
     node* next;
     node(int data){
         this->data=data;
+        this->next=NULL;
     }
 };
 
@@ -14,21 +15,41 @@ node* takeInput(){
 int data;
 cin>>data;
 node* head=NULL;
+node* tail=NULL;
 while(data!=-1){
     node* newnode=new node(data);
     if(head==NULL){
         head=newnode;
+        tail=newnode;
     }
     else{
-        node* temp=head;
-        while(temp->next!=NULL){
-            temp=temp->next;
-        }
-        temp->next=newnode;
+        tail->next=newnode;
+        tail=tail->next;
     }
     cin>>data;
 }
 return head;
+}
+
+void insert_node(node* head, int i,int data){
+    node* newnode=new node(data);
+    int count=0;
+    node* temp=head;
+    while(count<i-2){
+        temp=temp->next;
+        count++;
+    }
+    node* a=temp->next;
+    newnode->next=a;
+    temp->next=newnode;
+}
+
+void delete_node(node* head,int data){
+    int count=0;
+    node* temp=head;
+    while(data!=temp->data){
+        temp=temp->next;
+    }
 }
 
 void print(node* head){
@@ -40,6 +61,12 @@ void print(node* head){
 int main(){
 
     node* head=takeInput();
+    
+    print(head);
+    int i;
+    int data;
+    cin>>i>>data;
+    insert_node(head,i,data);
     print(head);
 
     return 0;
